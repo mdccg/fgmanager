bcb8dbf29d427cae513a653ec59a0ed875ddff59
mdccg <matheusdanielgomess@gmail.com>
2020-05-20 01:14:04 -0400

    🌌 e foi por ela que eu comecei a estudar cosmologia...

--- frontend/src/pages/CadastroProdutos/index.js
+++ frontend/src/pages/CadastroProdutos/index.js
@@ -7,54 +7,34 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import api from './../../services/api';
 
-const Form = props => (
-  <div className="form">
-    <form>
-      <div className="row">
-        <div className="col">
-          <TextField name="codigoAparelho" id="outlined-basic" label="Código" variant="outlined" />
-        </div>
-
-        <div className="col">
-          <TextField name="codigoSmartCard" id="outlined-basic" label="Smart card" variant="outlined" />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <TextField name="nome" id="outlined-basic" label="Nome" variant="outlined" />
-        </div>
-
-        <div className="col">
-          <Autocomplete
-            id="combo-box-demo"
-            options={props.modelos.map(modelo => ({ title: modelo.nome, modelo: modelo._id }))}
-            getOptionLabel={(option) => option.title}
-            renderInput={(params) => <TextField {...params} name="modelo" id="outlined-basic" label="Modelo" variant="outlined" />}
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <TextField name="marca" id="outlined-basic" label="Marca" variant="outlined" />
-        </div>
-
-        <div className="col">
-          <input type="submit" value="Adicionar" />
-        </div>
-      </div>
-    </form>
-  </div>
-);
-
 class CadastroProdutos extends Component {
-  state = { modelos: [] };
+  state = {
+    modelos: []
+  };
+
+  cadastrar = event => {
+    event.preventDefault();
+
+    const atributos = ['nome', 'marca', 'modelo', 'codigo', 'smartCard'],
+        { modelos } = this.state,
+          produto = {};
+    
+    for(const atributo of atributos)
+      produto[atributo] = event.target[atributo].value;
+
+    const modelo = modelos.filter(modelo => modelo.nome === produto.modelo)[0];
+    
+    produto.modelo = modelo._id;
+
+    api.post('/estoque/produto/novo', produto)
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+  }
 
   async componentDidMount() {
-    const modelos = await api.get('/estoque/modelo');
+    const response = await api.get('/estoque/modelo');
     
-    this.setState({ modelos: modelos.data });
+    this.setState({ modelos: response.data });
   }
 
   render() {
@@ -72,7 +52,44 @@ class CadastroProdutos extends Component {
           </Link>
         </section>
 
-        <Form modelos={modelos} />
+        <div className="form">
+          <form onSubmit={event => this.cadastrar(event)}>
+            <div className="row">
+              <div className="col">
+                <TextField name="codigo" id="outlined-basic" label="Código" variant="outlined" required />
+              </div>
+
+              <div className="col">
+                <TextField name="smartCard" id="outlined-basic" label="Smart card" variant="outlined" required />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <TextField name="nome" id="outlined-basic" label="Nome" variant="outlined" required />
+              </div>
+
+              <div className="col">
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={modelos.map(modelo => ({ nome: modelo.nome, _id: modelo._id }))}
+                  getOptionLabel={(option) => option.nome}
+                  renderInput={(params) => <TextField {...params} name="modelo" id="outlined-basic" label="Modelo" variant="outlined" required />}
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <TextField name="marca" id="outlined-basic" label="Marca" variant="outlined" required />
+              </div>
+
+              <div className="col">
+                <input type="submit" value="Adicionar" />
+              </div>
+            </div>
+          </form>
+        </div>
       </main>
     );
   }
