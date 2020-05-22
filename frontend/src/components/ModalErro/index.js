0c595fe232e44a36eed58cd4e0c3d107307b562b
Rafael Castro <rafael.castro1002@gmail.com>
2020-05-21 22:36:28 -0400

    Adicionado modal de erro

---
+++ frontend/src/components/ModalErro/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalHeader,
+  MDBModalBody,
+  MDBModalFooter
+} from 'mdbreact';
+import './index.css';
+
+class Teste extends Component {
+  state = {
+    modal: false,
+    message: 'Deu pau...'
+  };
+
+  toggle = message => {
+    this.setState({
+      modal: !this.state.modal,
+      message: message
+    });
+
+    setTimeout(() => {
+      this.setState({ modal: !this.state.modal });
+
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <main>
+        <MDBModal isOpen={this.props.mensagem} position="center" toggle={() => this.props.toggle()} side position="bottom-right">
+          <MDBModalHeader className="size-padding-modal-erro modal-header-erro-background"> <a className="title-header-modal-erro" onClick={() => this.props.toggle()}> <i class="far fa-times-circle"></i> </a> </MDBModalHeader>
+          <MDBModalBody className="text-mensagem">
+            {this.props.mensagem}
+          </MDBModalBody>
+          <MDBModalFooter className="size-padding-modal-erro">
+            <MDBBtn color="danger" size="sm" onClick={() => this.props.toggle()}>fechar</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+
+        
+      </main>
+    );
+  }
+}
+
+export default Teste;
--- frontend/src/pages/Clientes/index.js
+++ frontend/src/pages/Clientes/index.js
@@ -3,40 +3,55 @@ import './styles.css';
 
 import api from './../../services/api';
 
-function cadastrar(event) {
-  event.preventDefault();
-  
-  const form = {
-    nome: document.getElementById("inome").value,
-    cpf: document.getElementById("icpf").value,
-    rg: document.getElementById("irg").value,
-    telefone: document.getElementById("itel").value,
-    email: document.getElementById("iemail").value,
-    endereco: {
-      rua: document.getElementById("irua").value,
-      numero: document.getElementById("inum_rua").value,
-      bairro: document.getElementById("ibairro").value,
-      cidade: document.getElementById("icidade").value,
-      cep: document.getElementById("icep").value,
-      pontoReferencia: document.getElementById("iponto_referencia").value,
-    }
-  }
+import ModalErro from '../../components/ModalErro';
 
-  api.post('/clientes/novo', form)
-    .catch(error => {
-      const { mensagem } = error.response.data;
-      console.log(mensagem); // TODO implementar modal aqui...
-      
-      /* https://material-ui.com/pt/components/modal/ */
-    });
-}
 
 
 class Clientes extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mensagemErro: null
+    }
+  }
+
+  cadastrar(event) {
+    event.preventDefault();
+
+    const form = {
+      nome: document.getElementById("inome").value,
+      cpf: document.getElementById("icpf").value,
+      rg: document.getElementById("irg").value,
+      telefone: document.getElementById("itel").value,
+      email: document.getElementById("iemail").value,
+      endereco: {
+        rua: document.getElementById("irua").value,
+        numero: document.getElementById("inum_rua").value,
+        bairro: document.getElementById("ibairro").value,
+        cidade: document.getElementById("icidade").value,
+        cep: document.getElementById("icep").value,
+        pontoReferencia: document.getElementById("iponto_referencia").value,
+      }
+    }
+
+    api.post('/clientes/novo', form)
+      .catch(error => {
+        const { mensagem } = error.response.data;
+        this.setState({ mensagemErro: mensagem })
+
+        /* https://material-ui.com/pt/components/modal/ */
+      });
+  }
+
+  toggleMensagemErro() {
+    this.setState({ mensagemErro: null })
+  }
+
   render() {
     return (
       <main>
-        <form onSubmit={(event) => cadastrar(event)}>
+        <form onSubmit={(event) => this.cadastrar(event)}>
           <div className="clientes">
             <div>
               <div className="containerTitulo">
@@ -55,63 +70,64 @@ class Clientes extends Component {
                 <div className="row">
                   <div className="col-lg-4">
                     <label className="rotulo">Nome completo</label>
-                    <input type="text" name="nome" id="inome" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="nome" id="inome" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Nº de CPF</label>
-                    <input type="text" name="cpf" id="icpf" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="cpf" id="icpf" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Nº de RG</label>
-                    <input type="text" name="rg" id="irg" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="rg" id="irg" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-4">
                     <label className="rotulo">Telefone</label>
-                    <input type="text" name="tel" id="itel" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="tel" id="itel" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">E-Mail</label>
-                    <input type="text" name="email" id="iemail" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="email" id="iemail" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Rua</label>
-                    <input type="text" name="rua" id="irua" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="rua" id="irua" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-4">
                     <label className="rotulo">Nº de Rua</label>
-                    <input type="text" name="num_rua" id="inum_rua" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="num_rua" id="inum_rua" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Bairro</label>
-                    <input type="text" name="bairro" id="ibairro" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="bairro" id="ibairro" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Cidade</label>
-                    <input type="text" name="cidade" id="icidade" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="cidade" id="icidade" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
 
                 </div>
                 <div className="row ">
                   <div className="col-lg-4 ">
                     <label className="rotulo">CEP</label>
-                    <input type="text" name="cep" id="icep" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="cep" id="icep" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4">
                     <label className="rotulo">Ponto de referencia</label>
-                    <input type="text" name="ponto_referencia" id="iponto_referencia" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required/>
+                    <input type="text" name="ponto_referencia" id="iponto_referencia" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" required />
                   </div>
                   <div className="col-lg-4 botao">
-                    <button type="submit" name="adicionar" id="iadicionar" className=" btn btn-block btn-success ">ADICIONAR</button>
+                    <button type="submit" name="adicionar" id="iadicionar" className=" btn btn-block btn-success btn-adicionar-clientes">ADICIONAR</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </form>
+        <ModalErro mensagem={this.state.mensagemErro} toggle={() => this.toggleMensagemErro()} />
       </main>
     );
   }
